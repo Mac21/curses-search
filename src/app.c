63654ac3438dcc215e4c2b221a8ff8f07fad4f7c
Mac21 <russ.tolle@gmail.com>
2018-01-16 17:23:20 -0700

    Add work to init maze from file.

--- src/app.c
+++ src/app.c
@@ -1,7 +1,25 @@
+#include "stdlib.h"
 #include "ncurses.h"
 
 #include "app.h"
 #include "maze.h"
+
+char** load_maze(char* path) {
+  char** maze = calloc(COLS, sizeof(char*));
+  char* line = calloc(LINES, sizeof(char));
+
+  FILE* file = fopen(path, "r");
+  for(size_t i = 0; i < sizeof(maze); i++) {
+    fgets(line, sizeof line, file);
+    if (line == NULL) {
+      break;
+    }
+    *(maze+i) = line;
+  }
+  free(file);
+
+  return maze;
+}
 
 void run() {
   initscr();
@@ -9,11 +27,21 @@ void run() {
   noecho();
   box(stdscr, ACS_VLINE, ACS_HLINE);
 
+  char** maze = load_maze("./maze.txt");
+
   int done = 0;
-  while(!done) {
+  //while(!done) {
+  for (size_t i = 0; i < sizeof(maze); i++) {
+    waddstr(stdscr, *(maze + i));
     refresh();
-    draw_maze(stdscr, 1, 1, 2);
   }
 
+  while(!done) {
+    refresh();
+  }
+    //draw_maze(stdscr, 1, 1, 2);
+  //}
+
+  free(maze);
   endwin();
 }
--- src/maze.c
+++ src/maze.c
@@ -4,14 +4,18 @@
 #include "maze.h"
 
 void draw_maze(WINDOW* win, size_t startx, size_t starty, size_t tile_width) {
-  size_t endx = LINES - tile_width;
-  size_t endy = COLS - tile_width;
+  size_t endx = COLS - tile_width;
+  size_t endy = LINES - tile_width;
 
-  for(size_t i = startx; i <= endx; i++) {
-    for(size_t j = starty; j <= endy; j++) {
+  for(size_t i = starty; i <= endy; i++) {
+    for(size_t j = startx; j <= endx; j++) {
       mvwaddch(win, i, j, '#');
     }
   }
+  mvwaddch(win, 20, 20, '#');
+
+  char temp = (mvwinch(win, 20, 20) & A_CHARTEXT);
+  mvwaddch(win, endy - 1, endx - 2, temp);
 
   wrefresh(win);
 }
